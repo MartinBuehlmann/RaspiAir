d2bc3d1ecd4cd31758227e61d5701df82125bace
Martin Bühlmann <martin.buehlmann@erowa.com>
2025-03-10 07:20:18 +0100

    Subscribe to event and read latest Co2Concentration.

--- source/RaspiAir.Co2TrafficLight/Co2TrafficLightObserverService.cs
+++ source/RaspiAir.Co2TrafficLight/Co2TrafficLightObserverService.cs
@@ -2,16 +2,44 @@
 
 using System.Threading;
 using System.Threading.Tasks;
+using EventBroker;
+using RaspiAir.Reporting;
+using RaspiAir.Reporting.Domain;
+using RaspiAir.Reporting.Events;
 using RaspiRobot.Lights.Common;
 
-// TODO: Listen to MeasurementReportUpdatedEvent and update LEDs respectively
-internal class Co2TrafficLightObserverService : IBackgroundService
+internal class Co2TrafficLightObserverService :
+    IBackgroundService,
+    IEventSubscriptionAsync<MeasurementReportUpdatedEvent>
 {
+    private readonly EventSubscriber eventSubscriber;
+    private readonly IReportingRepository repository;
+
+    public Co2TrafficLightObserverService(
+        EventSubscriber eventSubscriber,
+        IReportingRepository repository)
+    {
+        this.eventSubscriber = eventSubscriber;
+        this.repository = repository;
+    }
+
     public int Order => int.MaxValue;
 
     public Task StartAsync(CancellationToken cancellationToken)
-        => Task.CompletedTask;
+    {
+        this.eventSubscriber.Subscribe(this);
+        return Task.CompletedTask;
+    }
 
     public Task StopAsync(CancellationToken cancellationToken)
-        => Task.CompletedTask;
+    {
+        this.eventSubscriber.Unsubscribe(this);
+        return Task.CompletedTask;
+    }
+
+    // TODO: Update LEDs according value rating.
+    public async Task HandleAsync(MeasurementReportUpdatedEvent data)
+    {
+        Co2Concentration co2Concentration = await this.repository.RetrieveLatestCo2ConcentrationAsync();
+    }
 }
