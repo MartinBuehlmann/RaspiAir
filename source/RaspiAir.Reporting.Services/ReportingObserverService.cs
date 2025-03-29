0f82feaf48bd29886e83cac66faabdbc322844d7
Martin Bühlmann <martin.buehlmann@gmx.ch>
2025-03-29 17:05:43 +0100

    Remove console logging of ReportingObserverService and apply small performance improvement by reducing async task state machine usage.

--- source/RaspiAir.Reporting.Services/ReportingObserverService.cs
+++ source/RaspiAir.Reporting.Services/ReportingObserverService.cs
@@ -1,6 +1,5 @@
 ﻿namespace RaspiAir.Reporting.Services;
 
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Common.BackgroundServices;
@@ -17,8 +16,6 @@ internal class ReportingObserverService(
         IEventSubscriptionAsync<HumidityChangedEvent>,
         IEventSubscriptionAsync<Co2ConcentrationChangedEvent>
 {
-    private readonly Lock locker = new();
-
     public int Order => 5;
 
     public Task StartAsync(CancellationToken cancellationToken)
@@ -33,41 +30,12 @@ internal class ReportingObserverService(
         return Task.CompletedTask;
     }
 
-    public async Task HandleAsync(TemperatureChangedEvent data)
-    {
-        await repository.SaveAsync(new TemperatureMeasurementEntity(data.Temperature, data.Timestamp));
-        this.WriteToConsole(() =>
-            Console.WriteLine($"{data.Timestamp.LocalDateTime}: Temperature: {data.Temperature:N1}C"));
-    }
+    public Task HandleAsync(TemperatureChangedEvent data)
+        => repository.SaveAsync(new TemperatureMeasurementEntity(data.Temperature, data.Timestamp));
 
-    public async Task HandleAsync(HumidityChangedEvent data)
-    {
-        await repository.SaveAsync(new HumidityMeasurementEntity(data.Humidity, data.Timestamp));
-        this.WriteToConsole(() => Console.WriteLine($"{data.Timestamp.LocalDateTime}: Humidity   : {data.Humidity:N1}%"));
-    }
+    public Task HandleAsync(HumidityChangedEvent data)
+        => repository.SaveAsync(new HumidityMeasurementEntity(data.Humidity, data.Timestamp));
 
-    public async Task HandleAsync(Co2ConcentrationChangedEvent data)
-    {
-        await repository.SaveAsync(new Co2ConcentrationMeasurementEntity(data.Co2Concentration, data.Timestamp));
-        this.WriteToConsole(() =>
-        {
-            Console.Write($"{data.Timestamp.LocalDateTime}: CO2        : ");
-            Console.ForegroundColor =
-                data.Co2Concentration < 400 ? ConsoleColor.DarkGreen :
-                data.Co2Concentration < 1000 ? ConsoleColor.Green :
-                data.Co2Concentration < 2000 ? ConsoleColor.Yellow :
-                data.Co2Concentration < 4000 ? ConsoleColor.DarkYellow :
-                ConsoleColor.Red;
-            Console.WriteLine($"{data.Co2Concentration}ppm");
-            Console.ResetColor();
-        });
-    }
-
-    private void WriteToConsole(Action action)
-    {
-        lock (this.locker)
-        {
-            action();
-        }
-    }
+    public Task HandleAsync(Co2ConcentrationChangedEvent data)
+        => repository.SaveAsync(new Co2ConcentrationMeasurementEntity(data.Co2Concentration, data.Timestamp));
 }
