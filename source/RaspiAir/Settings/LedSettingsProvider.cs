dbdd2dab73ec6bc014b8a9a214e9dd80d450c948
Martin Bühlmann <martin.buehlmann@gmx.ch>
2025-03-30 13:25:08 +0200

    Remove led count configuration because it is internally fixed to 8 anyway.

--- source/RaspiAir/Settings/LedSettingsProvider.cs
+++ source/RaspiAir/Settings/LedSettingsProvider.cs
@@ -1,21 +1,10 @@
 ﻿namespace RaspiAir.Settings;
 
 using System.Threading.Tasks;
-using DocumentStorage;
 using LedStripe.Control.Settings;
 
-internal class LedSettingsProvider(IDocumentStorage documentStorage) : ILedSettingsProvider
+internal class LedSettingsProvider : ILedSettingsProvider
 {
-    private const string SettingsFileName = nameof(LedSettings);
-
-    public async Task<LedSettings> ProvideAsync()
-        => await documentStorage.ReadAsync<LedSettings>(SettingsFileName) ??
-           await this.CreateAsync(SettingsFileName);
-
-    private async Task<LedSettings> CreateAsync(string settingsFileName)
-    {
-        var defaultStoragesSettings = new LedSettings(8);
-        await documentStorage.WriteAsync(defaultStoragesSettings, settingsFileName);
-        return defaultStoragesSettings;
-    }
+    public Task<LedSettings> ProvideAsync()
+        => Task.FromResult(new LedSettings(8));
 }
